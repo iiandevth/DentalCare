01bf4ef76e0f8fa542541406e14c9aeb696348bd
Dew <andrelaurenceg@gmail.com>
2025-03-05 17:46:39 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware('verified')->name('dashboard');
+
+    Route::get('/buttons', function () {
+        return view('buttons');
+    })->name('buttons');
+
+    Route::get('/cards', function () {
+        return view('cards');
+    })->name('cards');
+
+    Route::get('/charts', function () {
+        return view('charts');
+    })->name('charts');
+
+    Route::get('/forms', function () {
+        return view('forms');
+    })->name('forms');
+
+    Route::get('/modals', function () {
+        return view('modals');
+    })->name('modals');
+
+    Route::get('/tables', function () {
+        return view('tables');
+    })->name('tables');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
